48147714c0493fca35f078be4c9be4240885fe3c
Kieran Millar <kieran@millar.org>
2019-04-23 21:53:09 +0100

    Automatic hinting

--- scripts/noncombats.js
+++ scripts/noncombats.js
@@ -1,3 +1,5 @@
+var nonComHints = [];
+
 var noncombats = [
 	{
 		id: 0,
@@ -5,10 +7,9 @@ var noncombats = [
 		description: "You get a rare quiet moment to yourself, and take the opportunity to look at things a little more closely.\n",
 		choices: [
 			{
-				buttonText: function () {return noncombatButton ("Look on the desk", nonComHintsEnum.TOWNHALLBASEMENT1, "Once: get town hall key; otherwise: get health potion");},
+				buttonText: function () {return noncombatButton ("Look on the desk", 0, "Once: get town hall key; otherwise: get health potion");},
 				onChoosing: function ()
 				{
-					nonComHints[nonComHintsEnum.TOWNHALLBASEMENT1] = 1;
 					if (player.questTownHall == 1)
 					{
 						player.questTownHall = 2;
@@ -23,20 +24,18 @@ var noncombats = [
 				}
 			},
 			{
-				buttonText: function () {return noncombatButton ("Look on the shelves", nonComHintsEnum.TOWNHALLBASEMENT2, "get dusty ring");},
+				buttonText: function () {return noncombatButton ("Look on the shelves", 1, "get dusty ring");},
 				onChoosing: function ()
 				{
-					nonComHints[nonComHintsEnum.TOWNHALLBASEMENT2] = 1;
 					addNoncombatText ("You look on the shelves on the wall. There is row after row of dusty books, dusty tools, and all sorts of dusty trinkets. All of them seem useless, but a slightly glowing ring catches your eye.");
 					addNoncombatText ("It appears to be an enchanted ring, but the layers of dust that have accumulated over the years have magically fused to the ring, and become part of it. Still, it might have some use left in it.");
 					getNoncombatItem (10, 1);
 				}
 			},
 			{
-				buttonText: function () {return noncombatButton ("Look for trouble", nonComHintsEnum.TOWNHALLBASEMENT3, "fight a monster");},
+				buttonText: function () {return noncombatButton ("Look for trouble", 2, "fight a monster");},
 				onChoosing: function ()
 				{
-					nonComHints[nonComHintsEnum.TOWNHALLBASEMENT3] = 1;
 					let r = Math.random();
 					r = Math.floor (r * zones[1].combats.length);
 					beginCombat (combats[zones[1].combats[r]]);
@@ -50,20 +49,18 @@ var noncombats = [
 		description: "You manage to sneak into a calm corner of the office. Here you find three sturdy cardboard boxes in front of you, each one oozing terrifying levels of boredom. You figure you have time to loot one of them before the sheer boredom of it all forces you to make a hasty retreat.\n",
 		choices: [
 			{
-				buttonText: function () {return noncombatButton ("Open the dull, white box", nonComHintsEnum.TOWNHALLTAXOFFICE1, "30 gold");},
+				buttonText: function () {return noncombatButton ("Open the dull, white box", 0, "30 gold");},
 				onChoosing: function ()
 				{
-					nonComHints[nonComHintsEnum.TOWNHALLTAXOFFICE1] = 1;
 					addNoncombatText ("You open the brown box and find an unmarked envelope. Unable to contain yourself from the excitement of finding another sealed container inside the first one, you tear it open, and gold goes flying everywhere! Looks like somebody tried to pay their taxes with cash.");
 					addNoncombatText ("You scramble around to pick up as much gold as you can, and only manage to pick up a small amount before boredom settles back in. You quickly leave before you go mad.");
 					addNoncombatText (giveGold(30, false));
 				}
 			},
 			{
-				buttonText: function () {return noncombatButton ("Open the dull, grey box", nonComHintsEnum.TOWNHALLTAXOFFICE2, "15 exp, -1 HP");},
+				buttonText: function () {return noncombatButton ("Open the dull, grey box", 1, "15 exp, -1 HP");},
 				onChoosing: function ()
 				{
-					nonComHints[nonComHintsEnum.TOWNHALLTAXOFFICE2] = 1;
 					addNoncombatText ("You go to open the grey box, and find it to be sealed shut! Well, this is certainly not going to stop you and you decide to open it with brute force. You struggle to pick up the box, and slowly raise it over your head, before throwing it down again onto the ground, right onto your toe!");
 					addNoncombatText ("Muttering under your breath, you hobble out of the room, but at least you got a workout.");
 					addNoncombatText (giveExp(15));
@@ -72,10 +69,9 @@ var noncombats = [
 				}
 			},
 			{
-				buttonText: function () {return noncombatButton ("Open the dull, brown box", nonComHintsEnum.TOWNHALLTAXOFFICE3, "get cardboard panel");},
+				buttonText: function () {return noncombatButton ("Open the dull, brown box", 2, "get cardboard panel");},
 				onChoosing: function ()
 				{
-					nonComHints[nonComHintsEnum.TOWNHALLTAXOFFICE3] = 1;
 					addNoncombatText ("You try to open the brown box, but it's sealed shut with tape. Given that it's only cardboard, you give it a big tug, and end up ripping a big chunk off of the box.");
 					addNoncombatText ("Turns out the box was empty, but hey, you got some sweet free cardboard! There's treasure everywhere!");
 					getNoncombatItem (7, 1);
@@ -85,9 +81,12 @@ var noncombats = [
 	},
 ];
 
-function noncombatButton (text, hint, hintText)
+function noncombatButton (text, choice, hintText)
 {
-	if (nonComHints[hint] == 1)
+	if (nonComHints[currentNoncom] == undefined) {
+		nonComHints[currentNoncom] = [];
+	}
+	if (nonComHints[currentNoncom][choice] == 1)
 	{
 		text += " (" + hintText + ")";
 	}
@@ -102,6 +101,7 @@ function noncombatChoice (c)
 	$("#noncombatText").empty();
 	busy = false;
 	noncombats[currentNoncom].choices[c].onChoosing();
+	nonComHints[currentNoncom][c] = 1;
 	$("#noncombatButtons").empty();
 	if (busy == true)
 		return;
